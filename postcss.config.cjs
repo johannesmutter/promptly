051c55e01d7f5c4c4ddbb697d2b515059722fe1a
Johannes <info@johannesmutter.de>
2023-03-31 22:07:08 +0200

    added postcss plugins

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,50 @@
 const autoprefixer = require('autoprefixer');
+const postcssNesting = require('postcss-nesting');
+const postcssNormalize = require('postcss-normalize')
+const postcssPresetEnv = require('postcss-preset-env');
+const cssnano = require('cssnano');
+const postcssImport = require("postcss-import");
+const mixins = require('postcss-mixins');
+const colorFunction = require("postcss-color-function")
+const flexbugs = require('postcss-flexbugs-fixes');
+const easingGadients = require('postcss-easing-gradients');
+const colorguard = require('colorguard');
 
 const config = {
-	plugins: [autoprefixer]
+	plugins: [
+		postcssImport,
+
+		mixins,
+
+		postcssNesting,
+
+		// Apply modern CSS features with fallbacks
+		postcssPresetEnv,
+
+		// Normalize CSS
+		postcssNormalize,
+
+		colorFunction,
+
+		easingGadients,
+
+		// Flexbox bug fixes
+		flexbugs,
+
+		// Add vendor prefixes
+		autoprefixer,
+
+		// Check for color clashes. To use colorguard: run "npx colorguard styles.css". This will output a report showing any color clashes and suggestions for alternative colors.
+		colorguard,
+
+		// minimize css
+		// @CSS BUGs: in case of unexpected CSS behaviour disable this.
+		cssnano({
+			preset: ['default', {
+				discardComments: { removeAll: true },
+			}]
+		}),
+	]
 };
 
 module.exports = config;
