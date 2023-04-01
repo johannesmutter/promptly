1bb9253b52f314d81dd8461e58b28412a86fd447
Johannes <info@johannesmutter.de>
2023-04-02 01:58:55 +0200

    use immer for block insert function; fix bugs

--- src/lib/stores/blocks.js
+++ src/lib/stores/blocks.js
@@ -26,9 +26,7 @@ export const rootBlock = writable("cdfd5205-aade-4847-8789-487e41d7ff3f")
 
 
 function createBlockStore() {
-	/**
-	 * @type {import('svelte/store').Writable<BlockStore>}
-	 */
+	/** @type {import('svelte/store').Writable<BlockStore>} */
   const { subscribe, set, update } = writable(sampleData);
 
   return {
@@ -44,11 +42,8 @@ function createBlockStore() {
 		updateChildText: ( parentID, indexInChildren, newText) => {
       update(store =>
         produce(store, draftStore => {
-          if (
-            draftStore[parentID] &&
-            draftStore[parentID].children &&
-            draftStore[parentID].children[indexInChildren]
-          ) {
+          if (draftStore?.[parentID]?.children?.[indexInChildren] ) {
+            // @ts-ignore
             draftStore[parentID].children[indexInChildren].text = newText;
           }
         })
@@ -88,92 +83,61 @@ function createBlockStore() {
 				blockRef = {id: blockID};
 			}
 
-			update(store => {
+			update(store => produce(store, draftStore => {
 
+				const isValidParentID = parentID && uuidValidateV4(parentID);
+				const isValidIndexInChildren = typeof indexInChildren !== 'undefined' && Number.isInteger(indexInChildren) && indexInChildren >= 0;
+				const isValidCaretOffset = typeof caretOffset !== 'undefined' && Number.isInteger(caretOffset) && caretOffset >= 0;
+		
 				// Case 1: has type, but no parentID, add block to root of store.
-				if (
-					indexInChildren && typeof indexInChildren !== 'number' && indexInChildren < 0 &&
-					caretOffset && typeof caretOffset !== 'number' &&  caretOffset < 0 &&
-					(!parentID || !uuidValidateV4(parentID)) &&
-					blockID
-				) {
-					return {
-						...store, 
-						[blockID]: data
-					}
+				if (!isValidParentID && blockID) {
+					// Case 1: has type, but no parentID, add block to root of store.
+					draftStore[blockID] = data;
+					return;
 				}
-
+		
+				if (!isValidParentID) {
+					return;
+				}
+		
+				const parentData = draftStore[parentID];
+		
 				// Case 2: parentID and indexInChildren are defined, add block to parent.
-				if (
-					indexInChildren && typeof indexInChildren === 'number' && indexInChildren >= 0 &&
-					(!caretOffset || typeof caretOffset !== 'number' ||  caretOffset < 0) &&
-					parentID && uuidValidateV4(parentID)
+				if (parentData.children && isValidIndexInChildren && !isValidCaretOffset) {
+					const newChildren = [
+						...parentData.children.slice(0, indexInChildren),
+						// if block has type, add reference only
+						...(blockRef ? [blockRef] : [data]),
+						...parentData.children.slice(indexInChildren),
+					]
+					parentData.children = newChildren;
+		
+				} else if (
+					parentData.children &&
+					isValidIndexInChildren &&
+					isValidCaretOffset
 				) {
-					const parentData = store[parentID];
-					const newChildren = parentData.children
-						? [
-								...parentData.children.slice(0, indexInChildren),
-								// if block has type, add reference only
-								...(blockRef ? [blockRef] : [data]),
-								...parentData.children.slice(indexInChildren),
-							]
-						: [data];
-
-					return {
-						...store,
-						[parentID]: {
-							...parentData,
-							children: newChildren,
-						},
-						// if block has type, add data to root
-						...(blockRef ? {[blockID]: data} : {})
-					};
-				}
-	
-				// Case 3: parentID, indexInChildren and caretOffset are defined, add block after caretOffset at child of parent.
-				if (
-					typeof indexInChildren === 'number' && indexInChildren >= 0 &&
-					typeof caretOffset === 'number' &&  caretOffset >= 0 &&
-					parentID && uuidValidateV4(parentID)
-				) {
-					const parentData = store[parentID];
-					
-					if (!Array.isArray(parentData.children) || parentData.children.length === 0) {
-						return store;
+					// Case 3: parentID, indexInChildren, and caretOffset are defined, add block after caretOffset at child of parent.
+					const currentChild = parentData?.children?.[indexInChildren];
+					if (currentChild) {
+						const currentText = currentChild.text;
+						// TODO: in case of prompt, currentText does not exist at children, but can be found at root via id from "parentData.children[indexInChildren].id".
+						const beforeCaretText = currentText?.slice(0, caretOffset - (offsetBefore ?? 0));
+						const afterCaretText = currentText?.slice(caretOffset);
+		
+						parentData.children.splice(indexInChildren, 1,
+							{ ...currentChild, text: beforeCaretText },
+							...(blockRef ? [blockRef] : [data]),
+							{ ...currentChild, text: afterCaretText }
+						);
 					}
-					
-					const currentText = parentData.children[indexInChildren].text;
-					// TODO: in case of prompt, currentText does not exist at children, but can be found at root via id from "parentData.children[indexInChildren].id".
-					const beforeCaretText = currentText?.slice(0, caretOffset - (offsetBefore ?? 0));
-					const afterCaretText = currentText?.slice(caretOffset);
-
-					const childrenBefore = parentData.children.slice(0, indexInChildren);
-					const updatedChildBeforeCaret = { ...parentData.children[indexInChildren], text: beforeCaretText };
-					const updatedChildAfterCaret = { ...parentData.children[indexInChildren], text: afterCaretText };
-					const childrenAfter = parentData.children.slice(indexInChildren + 1);
-
-					return {
-						...store,
-						[parentID]: {
-							...parentData,
-							children: [
-								...childrenBefore,
-								updatedChildBeforeCaret,
-								// if block has type, add reference only
-								...(blockRef ? [blockRef] : [data]),
-								updatedChildAfterCaret,
-								...childrenAfter,
-							].filter(child => child !== null && child !== undefined),
-						},
-						// if block has type, add data to root
-						...(blockRef ? {[blockID]: data} : {})
-					};
 				}
-
-				// If none of the above casee is met, return old store
-				return store;
-			});
-
+		
+				if (blockRef) {
+					draftStore[blockID] = data;
+				}
+				
+			}))
 
 			if(blockID){
 				return blockID
