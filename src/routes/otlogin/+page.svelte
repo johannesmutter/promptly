99bf8d9efad9902c0a63457d5e486e391319f96f
Johannes <info@johannesmutter.de>
2023-04-16 16:08:47 +0200

    Style register page

--- src/routes/otlogin/+page.svelte
+++ src/routes/otlogin/+page.svelte
@@ -3,10 +3,22 @@
 </svelte:head>
 
 <main>
-	<div class="mx-auto max-w-screen-md">
+	<div>
+		<h2>Sign Up</h2>
 		<form action="?/register" method="POST" >
 			<input type="text" name="email" />
 			<button>Email Login Link</button>
 		</form>  
 	</div>
 </main>
+
+
+<style lang="postcss">
+	main {
+		padding: var(--size-4);
+		& div {
+			margin: 0 auto;
+			max-width: 50ex;
+		}
+	}
+</style>
