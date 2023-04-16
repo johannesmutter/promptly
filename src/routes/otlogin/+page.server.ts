53c1d50adf0af6a89f5e635e4c2c77d73400edee
Gukuma <eric@arteliers.work>
2023-04-16 19:13:44 +0200

    save prompt
    save prompt works
    load prompt doesnt

--- src/routes/otlogin/+page.server.ts
+++ src/routes/otlogin/+page.server.ts
@@ -11,24 +11,5 @@ export const actions: Actions = {
 
 		console.log("locals",locals)
 
-		// const { data, error: err } = await locals.supabase.auth.signInWithOtp({
-		// 	email: body.email as string,
-		// 	options: {
-		// 		emailRedirectTo: 'http://127.0.0.1:5173/',
-		// 	  },
-		// })
-
-		// if (err) {
-		// 	if (err instanceof AuthApiError && err.status === 400) {
-		// 		return fail(400, {
-		// 			error: "Invalid email or password",
-		// 		})
-		// 	}
-		// 	return fail(500, {
-		// 		error: "Server error. Please try again later.",
-		// 	})
-		// }
-
-		// throw redirect(303, "/")
 	},
 }
