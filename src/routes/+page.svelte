6e064af8a768fb6d5aee5f99dd9e1aca9eab784b
Johannes <info@johannesmutter.de>
2023-04-16 21:08:07 +0200

    Fix error "TypeError: Cannot read properties of null (reading 'user')". Format code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,25 @@
 <script lang="ts">
-	import { blocks, rootBlock } from "$lib/stores/blocks.js";
-	import Editor from "$lib/Editor.svelte";
+	import { blocks, rootBlock } from '$lib/stores/blocks.js';
+	import Editor from '$lib/Editor.svelte';
 	import { enhance, type SubmitFunction } from '$app/forms';
 	// import { supabaseClient } from '$lib/supabase';
 	import type { PageData } from './$types';
 	export let data: PageData;
 	import { onMount } from 'svelte';
-	import Icon from '$lib/Icon.svelte'
+	import Icon from '$lib/Icon.svelte';
 
-	import { supabase } from "$lib/supabaseClient";
-	let userid =data.session.user.id;
+	import { supabase } from '$lib/supabaseClient';
+	let userid = data?.session?.user?.id;
 
+	let blockslist = [];
 
-	let blockslist=[]
 	export async function load() {
-  const { data } = await supabase.from("prompts").select();
-  return data
-}
-
+		const { data } = await supabase.from('prompts').select();
+		return data;
+	}
 
 	const submitLogout: SubmitFunction = async (event) => {
-
-		console.log("logout",event)
+		console.log('logout', event);
 
 		// const { error } = await supabaseClient.auth.signOut();
 		// if (error) {
@@ -30,24 +28,17 @@
 		// cancel();
 	};
 
-	export async function savePrompt(user){
-
+	export async function savePrompt(user) {
 		const { data, error } = await supabase
-  .from('prompts')
-  .upsert({ user_id: user, prompt: $blocks?.[$rootBlock], id: $rootBlock})
+			.from('prompts')
+			.upsert({ user_id: user, prompt: $blocks?.[$rootBlock], id: $rootBlock });
 	}
 
-	
 	onMount(async () => {
-		blockslist = await load()
-  console.log(blockslist)
-})
-
+		blockslist = await load();
+		console.log(blockslist);
+	});
 </script>
-
-
-
-
 
 <header>
 	{#if data.session}
@@ -55,37 +46,23 @@
 			<button type="submit">Logout</button>
 		</form>
 	{:else}
-	
-		<a href="/otlogin" >login</a>
-
-		
-	{/if}			
+		<a href="/otlogin">login</a>
+	{/if}
 </header>
-
 
 <div>
 	{#if data.session}
-
-
-<section>
-	{#each blockslist as b}
-
-<div>
-	
-	<p><Icon src="message-spiral.svg" color="var(--violet-dark)" /> {b.prompt.text}</p>
-	
-</div>
-	
-	{/each}
-</section>
-	<Editor expanded={true} parentID={$rootBlock} />
-	<button on:click={() => savePrompt(userid)}>Save Prompt</button>
-
-		
+		<section>
+			{#each blockslist as b}
+				<div>
+					<p><Icon src="message-spiral.svg" color="var(--violet-dark)" /> {b.prompt.text}</p>
+				</div>
+			{/each}
+		</section>
+		<Editor expanded={true} parentID={$rootBlock} />
+		<button on:click={() => savePrompt(userid)}>Save Prompt</button>
 	{:else}
-
-	<Editor expanded={true} parentID={$rootBlock} />
-	
+		<Editor expanded={true} parentID={$rootBlock} />
 	{/if}
 </div>
 
